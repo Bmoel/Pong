243ed44db7b57ac3ba01ab1c227471e983d39791
Ben Moeller <benlynnmoeller@gmail.com>
2021-05-26 14:39:00 -0500

    5/26/2021 changes - 2:38 PM CT

--- game.js
+++ game.js
@@ -1,22 +1,43 @@
 let canvas;
 let context;
+let inMainMenu = true;
 
 document.addEventListener("DOMContentLoaded", () => {
     canvas = document.querySelector("#myCanvas");
     context = canvas.getContext("2d");
     main_menu();
-    draw();
 })
 
 function main_menu() {
-    
+    context.fillStyle = '#000000';
+    context.font = '25pt Times New Roman';
+    context.fillText("Pong", window.innerWidth/20, window.innerHeight/20);
+    var button = document.createElement("button");
+    button.innerHTML = "Play Game";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+    button.addEventListener("click", () => {
+        context.clearRect(0,0,canvas.width,canvas.height);
+        body.removeChild(button);
+        draw();
+    })
 }
 
 function draw() {
     context.canvas.width = window.innerWidth;
     context.canvas.height = window.innerHeight;
     print_constant_field();
+    player1();
+    computer_player();
     tick();
+}
+
+function player1() {
+
+}
+
+function computer_player() {
+    
 }
 
 function print_constant_field() {
